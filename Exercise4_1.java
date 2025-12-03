9964df465db03c5d40e0023571180cee44cc0e86
semaelmahmud <250541618@firat.edu.tr>
2025-12-03 21:46:25 +0300

    Refactor date printing methods to accept parameters correctly

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,24 @@
-public class Exercise4_1 {
+// Dosya adı: Main.java
+class Main {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+    public static void printAmerican(String day, String month, int date, int year) {
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    public static void printEuropean(String day, String month, int date, int year) {
+        // Örnek: Wednesday, 11 April 2007  -> 11 April 2007, Wednesday
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Wednesday";
+        String month = "April";
+        int date = 11;
+        int year = 2007;
+
+        printAmerican(day, month, date, year);
+        printEuropean(day, month, date, year);
     }
+}
+
 }
